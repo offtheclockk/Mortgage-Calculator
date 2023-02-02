f7c8c0b56932cfb991a0fedc61f391824ef18843
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-02-01 21:23:03 -0800

    changes

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.print("Rate: ");
-        double rate = (scanner.nextDouble() / 100)/12;
-        System.out.print("Period (Years): ");
-        int period = (scanner.nextInt() * 12);
+        var employee = new Employee(
+                50_000, 20);
+        Employee.printNumberOfEmployees();
+        int wage = employee.calculateWage(10);
 
-        double mortgage = principal * (rate * (Math.pow((1 + rate), period))) / (Math.pow((1 + rate), period) - 1);
-
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        System.out.println("Your mortgage payment will be " + currency.format(mortgage));
+        System.out.println(wage);
 
     }
 }
